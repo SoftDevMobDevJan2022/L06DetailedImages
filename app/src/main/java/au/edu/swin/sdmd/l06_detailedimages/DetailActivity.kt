4ad5b0465a9f14f3124b3762ae56e378dcf1906d
Duc Minh Le <lemduc@gmail.com>
2022-03-01 22:12:31 +0700

    completed code from the lecture video

--- app/src/main/java/au/edu/swin/sdmd/l06_detailedimages/DetailActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l06_detailedimages/DetailActivity.kt
@@ -2,6 +2,7 @@ package au.edu.swin.sdmd.l06_detailedimages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import l06_detailedimages.R
 
 class DetailActivity : AppCompatActivity() {
@@ -9,5 +10,28 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        // process the data passed in
+        /*version 1.0: pass primitive values
+        val vName = findViewById<TextView>(R.id.name)
+        val name = intent.getStringExtra("name")
+        vName.text = name
+
+        val vAuthor = findViewById<TextView>(R.id.author)
+        val author = intent.getStringExtra("author")
+        vAuthor.text = author
+        */
+
+        /**
+         * version 2.0: pass Parcelizable object
+         */
+        val location = intent.getParcelableExtra<Location>("location")
+
+        location?.let {
+            val vName = findViewById<TextView>(R.id.name)
+            vName.text = it.name
+
+            val vAuthor = findViewById<TextView>(R.id.author)
+            vAuthor.text = it.author
+        }
     }
 }
--- app/src/main/java/au/edu/swin/sdmd/l06_detailedimages/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l06_detailedimages/MainActivity.kt
@@ -1,5 +1,6 @@
 package au.edu.swin.sdmd.l06_detailedimages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -15,7 +16,24 @@ class MainActivity : AppCompatActivity() {
 
         val vStation = findViewById<TextView>(R.id.station)
         vStation.setOnClickListener {
+            // todo: create Intent
+            val intent = Intent(this, DetailActivity::class.java);
 
+            // pass data to the target activity
+            /* version 1.0: pass primitive values
+            intent.apply {
+                putExtra("name", station.name)
+                putExtra("author", station.author)
+            }
+             */
+
+            /* version 2.0: pass Parcelizable object */
+            intent.apply {
+                putExtra("location", station)
+            }
+
+            // show the detail-activity view
+            startActivity(intent)
         }
     }
 
